cb9aad8836a92006f31585c4f2acafd3d7bee783
Nullicorn <git@nullicorn.me>
2026-04-20 16:27:36 -0400

    Fix StormSerpentBone component not being added to the head

--- src/main/java/me/nullicorn/hytale/stormserpent/npc/action/ActionStormSerpentInitialize.java
+++ src/main/java/me/nullicorn/hytale/stormserpent/npc/action/ActionStormSerpentInitialize.java
@@ -10,6 +10,7 @@ import com.hypixel.hytale.server.npc.corecomponents.ActionBase;
 import com.hypixel.hytale.server.npc.role.Role;
 import com.hypixel.hytale.server.npc.sensorinfo.InfoProvider;
 import me.nullicorn.hytale.stormserpent.component.StormSerpent;
+import me.nullicorn.hytale.stormserpent.component.StormSerpentBone;
 import me.nullicorn.hytale.stormserpent.npc.action.builder.BuilderActionStormSerpentInitialize;
 import me.nullicorn.serpentine.asset.SerpentConfig;
 import me.nullicorn.serpentine.component.Serpent;
@@ -54,6 +55,7 @@ public final class ActionStormSerpentInitialize extends ActionBase {
         serpent = new Serpent(new Transform(transform.getPosition()), this.serpentConfig.layout().chooseBones());
         store.addComponent(ref, Serpent.getComponentType(), serpent);
         store.addComponent(ref, StormSerpent.getComponentType());
+        store.addComponent(ref, StormSerpentBone.getComponentType());
 
         return true;
     }
