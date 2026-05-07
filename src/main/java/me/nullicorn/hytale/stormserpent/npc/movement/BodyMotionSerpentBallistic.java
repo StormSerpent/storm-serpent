StormSerpent/storm-serpent

commit f69781f305ffa4b87365d4c816c3c28486e51070

Fix previous commit (1b41242c)
Previous "fix" was misinformed

src/main/java/me/nullicorn/hytale/stormserpent/npc/movement/BodyMotionSerpentBallistic.java
@@ -3,6 +3,7 @@ package me.nullicorn.hytale.stormserpent.npc.movement;
 import com.hypixel.hytale.component.ComponentAccessor;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.math.vector.Vector3dUtil;
+import com.hypixel.hytale.server.core.modules.debug.DebugUtils;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.asset.builder.BuilderSupport;
@@ -64,7 +65,7 @@ public final class BodyMotionSerpentBallistic extends BodyMotionBase {
         this.startPosition = new Vector3d(selfTransform.getPosition());
         this.targetPosition = new Vector3d(targetTransform.getPosition());
         this.targetHorizontalDistance = Vector2d.distance(this.startPosition.x, this.startPosition.z, this.targetPosition.x, this.targetPosition.z);
-        this.peakHorizontalDistance = this.targetHorizontalDistance * Math.sqrt(Math.abs(this.arcHeight)) / (Math.sqrt(Math.abs(this.arcHeight)) + Math.sqrt(Math.abs((this.targetPosition.y + this.arcHeight) - this.startPosition.y)));
+        this.peakHorizontalDistance = this.targetHorizontalDistance * Math.sqrt(Math.abs(this.arcHeight)) / (Math.sqrt(Math.abs(this.arcHeight)) + Math.sqrt(Math.abs(this.startPosition.y + this.arcHeight - this.targetPosition.y)));
         this.horizontalDirection = new Vector3d(this.targetPosition.x, 0, this.targetPosition.z).sub(this.startPosition.x, 0, this.startPosition.z).normalize();
         this.t = 0;
 
@@ -107,7 +108,7 @@ public final class BodyMotionSerpentBallistic extends BodyMotionBase {
         final double x1 = this.peakHorizontalDistance;
         final double x2 = this.targetHorizontalDistance;
         final double y0 = this.startPosition.y;
-        final double y1 = y0 + this.arcHeight;
+        final double y1 = this.targetPosition.y + this.arcHeight;
         final double y2 = this.targetPosition.y;
         final double l0 = ((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2));
         final double l1 = ((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2));
@@ -127,6 +128,7 @@ public final class BodyMotionSerpentBallistic extends BodyMotionBase {
         desiredSteering.setTranslation(new Vector3d(difference).normalize());
         desiredSteering.setTranslationRelativeSpeed(speed);
         desiredSteering.setRelativeTurnSpeed(this.relativeTurnSpeed);
+
         return true;
     }
 }

commit 00c9318ec8eca62e18d795237eea654eed3fea5d

Fix ballistic sometimes picking an upside down parabola
Fixes a regression from f69781f. When the serpent's launch position was higher than the target position + the arc height, the parabola chosen would arc downward and then back up into the sky, rather than the other way around

src/main/java/me/nullicorn/hytale/stormserpent/npc/movement/BodyMotionSerpentBallistic.java
@@ -108,7 +108,7 @@ public final class BodyMotionSerpentBallistic extends BodyMotionBase {
         final double x1 = this.peakHorizontalDistance;
         final double x2 = this.targetHorizontalDistance;
         final double y0 = this.startPosition.y;
-        final double y1 = this.targetPosition.y + this.arcHeight;
+        final double y1 = Math.max(this.startPosition.y, this.targetPosition.y) + this.arcHeight;
         final double y2 = this.targetPosition.y;
         final double l0 = ((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2));
         final double l1 = ((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2));

src/main/java/me/nullicorn/hytale/stormserpent/npc/movement/builder/BuilderBodyMotionSerpentBallistic.java
@@ -42,7 +42,7 @@ public final class BuilderBodyMotionSerpentBallistic extends BuilderBodyMotionBa
     @Override
     public Builder<BodyMotion> readConfig(final JsonElement data) {
         this.requireString(data, "TargetSlot", this.targetSlot, StringNotEmptyValidator.get(), BuilderDescriptorState.WorkInProgress, "Target to aim for", null);
-        this.requireDouble(data, "ArcHeight", this.arcHeight, null, BuilderDescriptorState.WorkInProgress, "Peak height of the arc relative to the target position", null);
+        this.requireDouble(data, "ArcHeight", this.arcHeight, null, BuilderDescriptorState.WorkInProgress, "Peak height of the arc relative to the starting OR target position, whichever is higher", null);
         this.requireDouble(data, "ArcPathSpeed", this.arcPathSpeed, DoubleSingleValidator.greaterEqual0(), BuilderDescriptorState.WorkInProgress, "Horizontal speed that the arc is followed at", null);
         this.getDouble(data, "RelativeSpeed", this.relativeSpeed, 1.0, DoubleSingleValidator.greaterEqual0(), BuilderDescriptorState.WorkInProgress, "Relative movement speed", null);
         this.getDouble(data, "RelativeTurnSpeed", this.relativeTurnSpeed, 1.0, DoubleSingleValidator.greaterEqual0(), BuilderDescriptorState.WorkInProgress, "Relative turning speed", null);